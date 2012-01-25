2a87b0a2bcbf5d186904c5e0299a24c139f7cad6
jedmundstephens <jedmundstephens@gmail.com>
2012-01-25 15:58:43 -0500

    Update questions.sql

--- questions.sql
+++ questions.sql
@@ -1,12 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 2.11.8.1deb5+lenny6
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Nov 16, 2011 at 11:51 AM
--- Server version: 5.0.51
--- PHP Version: 5.2.6-1+lenny10
-
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 --
